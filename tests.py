2171feebae527c22d31d25df8a2805c1dc8f0f1e
Gene Boggs <gene@ology.net>
2025-09-16 22:00:11 -0700

    WIP

--- tests.py
+++ tests.py
@@ -75,28 +75,11 @@ class TestMelodicDevice(unittest.TestCase):
         got = obj.grace_note(1, 'D5', offset=-1)
         self.assertEqual(got, expect)
 
-    #     expect = [['d6', 75], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 74], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 0)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 73], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','D#5'], ['d24','D5'], ['d24','C#5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
     #     expect = [['d24','C#5'], ['d24','D5'], ['d24','D#5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', -1)
-    #     self.assertEqual(got, expect)
-
-    #     expect = [['d24',75], ['d24',74], ['d24',73], ['d24',74]]
-    #     got = obj.turn('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',73], ['d24',74], ['d24',75], ['d24',74]]
-    #     got = obj.turn('qn', 74, -1)
     #     self.assertEqual(got, expect)
 
     #     expect = [['d24','D5'], ['d24','D#5'], ['d24','D5'], ['d24','D#5']]
@@ -106,13 +89,6 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.trill('qn', 'D5', 2, -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d24',74], ['d24',75], ['d24',74], ['d24',75]]
-    #     got = obj.trill('qn', 74, 2, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',73], ['d24',74], ['d24',73]]
-    #     got = obj.trill('qn', 74, 2, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','D5'], ['d24','D#5'], ['d48','D5']]
     #     got = obj.mordent('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
@@ -120,25 +96,11 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.mordent('qn', 'D5', -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d24',74], ['d24',75], ['d48',74]]
-    #     got = obj.mordent('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',73], ['d48',74]]
-    #     got = obj.mordent('qn', 74, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [ ['d24','D5'], ['d24','D#5'], ['d24','E5'], ['d24','F5'] ]
     #     got = obj.slide('qn', 'D5', 'F5')
     #     self.assertEqual(got, expect)
     #     expect = [ ['d24','D5'], ['d24','C#5'], ['d24','C5'], ['d24','B4'] ]
     #     got = obj.slide('qn', 'D5', 'B4')
-    #     self.assertEqual(got, expect)
-
-    #     expect = [ ['d24',74], ['d24',75], ['d24',76], ['d24',77] ]
-    #     got = obj.slide('qn', 74, 'F5')
-    #     self.assertEqual(got, expect)
-    #     expect = [ ['d24',74], ['d24',73], ['d24',72], ['d24',71] ]
-    #     got = obj.slide('qn', 74, 'B4')
     #     self.assertEqual(got, expect)
 
     # def test_C_major(self):
@@ -154,28 +116,11 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.grace_note('qn', 'D5', -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d6', 76], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 74], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 0)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 72], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','E5'], ['d24','D5'], ['d24','C5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
     #     expect = [['d24','C5'], ['d24','D5'], ['d24','E5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', -1)
-    #     self.assertEqual(got, expect)
-
-    #     expect = [['d24',76], ['d24',74], ['d24',72], ['d24',74]]
-    #     got = obj.turn('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',72], ['d24',74], ['d24',76], ['d24',74]]
-    #     got = obj.turn('qn', 74, -1)
     #     self.assertEqual(got, expect)
 
     #     expect = [['d24','D5'], ['d24','E5'], ['d24','D5'], ['d24','E5']]
@@ -185,25 +130,11 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.trill('qn', 'D5', 2, -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d24',74], ['d24',76], ['d24',74], ['d24',76]]
-    #     got = obj.trill('qn', 74, 2, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',72], ['d24',74], ['d24',72]]
-    #     got = obj.trill('qn', 74, 2, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','D5'], ['d24','E5'], ['d48','D5']]
     #     got = obj.mordent('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
     #     expect = [['d24','D5'], ['d24','C5'], ['d48','D5']]
     #     got = obj.mordent('qn', 'D5', -1)
-    #     self.assertEqual(got, expect)
-
-    #     expect = [['d24',74], ['d24',76], ['d48',74]]
-    #     got = obj.mordent('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',72], ['d48',74]]
-    #     got = obj.mordent('qn', 74, -1)
     #     self.assertEqual(got, expect)
 
     # def test_D_major(self):
@@ -219,28 +150,11 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.grace_note('qn', 'D5', -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d6', 76], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 74], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, 0)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d6', 73], ['d90', 74]]
-    #     got = obj.grace_note('qn', 74, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','E5'], ['d24','D5'], ['d24','C#5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
     #     expect = [['d24','C#5'], ['d24','D5'], ['d24','E5'], ['d24','D5']]
     #     got = obj.turn('qn', 'D5', -1)
-    #     self.assertEqual(got, expect)
-
-    #     expect = [['d24',76], ['d24',74], ['d24',73], ['d24',74]]
-    #     got = obj.turn('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',73], ['d24',74], ['d24',76], ['d24',74]]
-    #     got = obj.turn('qn', 74, -1)
     #     self.assertEqual(got, expect)
 
     #     expect = [['d24','D5'], ['d24','E5'], ['d24','D5'], ['d24','E5']]
@@ -250,25 +164,11 @@ class TestMelodicDevice(unittest.TestCase):
     #     got = obj.trill('qn', 'D5', 2, -1)
     #     self.assertEqual(got, expect)
 
-    #     expect = [['d24',74], ['d24',76], ['d24',74], ['d24',76]]
-    #     got = obj.trill('qn', 74, 2, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',73], ['d24',74], ['d24',73]]
-    #     got = obj.trill('qn', 74, 2, -1)
-    #     self.assertEqual(got, expect)
-
     #     expect = [['d24','D5'], ['d24','E5'], ['d48','D5']]
     #     got = obj.mordent('qn', 'D5', 1)
     #     self.assertEqual(got, expect)
     #     expect = [['d24','D5'], ['d24','C#5'], ['d48','D5']]
     #     got = obj.mordent('qn', 'D5', -1)
-    #     self.assertEqual(got, expect)
-
-    #     expect = [['d24',74], ['d24',76], ['d48',74]]
-    #     got = obj.mordent('qn', 74, 1)
-    #     self.assertEqual(got, expect)
-    #     expect = [['d24',74], ['d24',73], ['d48',74]]
-    #     got = obj.mordent('qn', 74, -1)
     #     self.assertEqual(got, expect)
 
 if __name__ == '__main__':
