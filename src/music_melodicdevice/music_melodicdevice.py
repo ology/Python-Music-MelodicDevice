3370c210d83dbf59cc2f45a7567bcb044c72958d
Gene Boggs <gene@ology.net>
2025-09-22 18:09:55 -0700

    WIP

--- src/music_melodicdevice/music_melodicdevice.py
+++ src/music_melodicdevice/music_melodicdevice.py
@@ -1,6 +1,4 @@
-import sys
-sys.path.append('./src')
-import music_melodicdevice.musical_scales as musical_scales
+import musical_scales
 from music21 import pitch, note
 import random
 import re
@@ -50,7 +48,7 @@ class Device:
             self.scale_name = name
         scale = []
         for i in range(-1,10):
-            s = musical_scales.scale(self.scale_note, self.scale_name, starting_octave=i)
+            s = musical_scales.scale(self.scale_note + str(i), self.scale_name)
             scale.append(s[:-1])
         scale = [ f"{x}" for y in scale for x in y ]
         if self.flat:
